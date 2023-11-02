85df044b3b93cf6458ee64f13e650c0b87bbf090
william <william19970923@gmail.com>
2023-11-02 11:58:24 +0800

    update code and interact to DB

---
+++ DB.sql
@@ -0,0 +1,5 @@
+-- write this into Mysql --
+CREATE TABLE news_list (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255)
+);
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #install package
 #pip3 install selenium
+#pip3 install pymysql
 #pip3 install time
 #pip3 install schedule
 
@@ -8,33 +9,30 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-#import pymysql
 import time
 import schedule
+import pymysql
+import pymysql.cursors
 
 #initailize two list
 last_title="" #default last_title is empty
 titles = []
 
+#connect to local DB
+conn = pymysql.connect(
+    host='',
+    port='',
+    user='',
+    passwd='',
+    db='',
+    charset='utf8'
+)
+
 #function of scrape the news
 def scrape_new_list():
-    #conn =pymysql.connect()
-
     driver = webdriver.Chrome() #should install webdriver first 
-
     URL = "https://www.nycu.edu.tw/nycu/ch/app/news/list?module=headnews&id=2994"
     driver.get(URL)
-
-    #connect to DB, currently failed by error code 1045
-    '''conn = pymysql.connect(
-        host='roundhouse.proxy.rlwy.net:50655',
-        port=50655,
-        user='root',
-        passwd='6hdeFc364fACf--eeBC2H5aE3-2BA-Hb',
-        db='railway',
-        charset='utf8'
-    )'''
-
     wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "newslist"))) #search all class name newslist
     
@@ -44,28 +42,41 @@ def scrape_new_list():
         element = driver.find_element(By.ID, id_value)
         title = element.get_attribute("title")
         titles.append(title)
-        print(title)
-
+        cursor =conn.cursor()
+        sql='insert into news_list(title) values(%s);'
+        data=[title]
+        try:
+            cursor.execute(sql,title)
+            conn.commit()
+            print("connection success")
+        except:
+            print("No connection")
+            print(cursor.error)
+            conn.rollback()
     driver.quit()
-    #conn.close()
-    
+    conn.close()
+
+#first time to scrapt the title
+def first_scrapt():
+    print("第一次標題")
+    scrape_new_list()
+
+#run the function
+first_scrapt()
+
 #function that check the news title is different or not 
 def review_title():
     global last_title
-    if not last_title:
-        print("第一次標題")
-        scrape_new_list()
-    elif set(titles) != set(last_title):
+    if set(titles) != set(last_title):
         print("標題有更新")
-        scrape_new_list()
+        scrape_new_list() #run the function
     else:
         print("無更新")
     
     last_title =titles
 
-#automatic check
-schedule.every(5).seconds.do(review_title)
-
+#run the function to automatic check
+schedule.every(3).days.do(review_title)
 
 while (1):
     schedule.run_pending()
